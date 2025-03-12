e909f9efda88b8f90d8811008a52a92e62c5462f
yiliu1237 <150484524+yiliu1237@users.noreply.github.com>
2025-03-12 17:00:58 -0400

    update mushroom_pos.json

--- script.js
+++ script.js
@@ -113,8 +113,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const period = getTimePeriod();
         currentSet = focusImages[period];
 
-        console.log(currentSet)
-
         if (currentIndex >= currentSet.length) {
             currentIndex = 0;
         }
@@ -142,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let mushroomPositions = {}; 
     // Load positions from JSON file
-    fetch("https://yiliu1237.github.io/Forest-Wander/mushroom_forest/mushroom_positions.json")
+    fetch("https://yiliu1237.github.io/Forest-Wander/mushroom_positions.json")
         .then(response => response.json())
         .then(data => {
             mushroomPositions = data;
